fb344e84c3ec3dc4d3258b7a855f16a70fc6b607
Matej Šándor <matejko.sandor@gmail.com>
2020-04-27 11:47:05 +0200

    added AsyncTask class in order to be able to manage multiple processes at the same time

--- app/src/main/java/com/sandor/top10downloaderapp/MainActivity.kt
+++ app/src/main/java/com/sandor/top10downloaderapp/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.sandor.top10downloaderapp
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
+    private val TAG1 = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG1,"onCreate: called")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private inner class DownloadData : AsyncTask<String,Void,String>() {
+        private val TAG = "DownloadData"
+
+        override fun doInBackground(vararg params: String?): String {
+            Log.d(TAG,"doInBackground: called")
+            TODO("Not yet implemented")
+        }
+
+        override fun onPostExecute(result: String?) {
+            Log.d(TAG,"onPostExecute: called")
+            super.onPostExecute(result)
+        }
     }
 }
