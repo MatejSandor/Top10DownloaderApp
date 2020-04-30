7654d511307d0bcf67260f49487a0d368579467f
Matej Šándor <matejko.sandor@gmail.com>
2020-04-30 07:22:03 +0200

    added arrayAdapter for the listView

--- app/src/main/java/com/sandor/top10downloaderapp/MainActivity.kt
+++ app/src/main/java/com/sandor/top10downloaderapp/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.sandor.top10downloaderapp
 
+import android.content.Context
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_main.*
 import java.net.URL
+import kotlin.properties.Delegates
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,15 +21,23 @@ class MainActivity : AppCompatActivity() {
 
         Log.d(TAG1, "onCreate: called")
 
-        val downloadData = DownloadData()
+        val downloadData = DownloadData(this, xmlListView)
         downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
 
         Log.d(TAG1, "onCreate: done")
     }
 
     companion object {
-        private class DownloadData : AsyncTask<String, Void, String>() {
+        private class DownloadData(context: Context, listView: ListView) : AsyncTask<String, Void, String>() {
             private val TAG = "DownloadData"
+
+            var propContext : Context by Delegates.notNull()
+            var propListView : ListView by Delegates.notNull()
+
+            init {
+                propContext = context
+                propListView = listView
+            }
 
             override fun doInBackground(vararg url: String?): String {
                 Log.d(TAG, "doInBackground: starts with ${url[0]}")
@@ -39,6 +52,9 @@ class MainActivity : AppCompatActivity() {
                 super.onPostExecute(result)
                 val parseApplications = ParseApplications()
                 parseApplications.parse(result)
+
+                val arrayAdapter = ArrayAdapter<FeedEntry>(propContext,R.layout.list_item, parseApplications.applications)
+                propListView.adapter = arrayAdapter
             }
 
             private fun downloadXML(urlPath: String?): String {
